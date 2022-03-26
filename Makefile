74b75935081ad00c7644400a051986c644516e6f
louis-br <46930119+louis-br@users.noreply.github.com>
2022-03-26 18:33:30 +0000

    Add WebServer

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	javac WebServer.java
+run: all
+	java WebServer.java
---
+++ WebServer.java
@@ -0,0 +1,16 @@
+import java.io.* ;
+import java.net.* ;
+import java.util.* ;
+
+public final class WebServer
+{
+    public static void main(String argv[]) throws Exception
+    {
+        System.out.println("Hello world!");
+    }
+}
+
+/*final class HttpRequest implements Runnable
+{
+    ...
+}*/
